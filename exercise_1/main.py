5f6eb7045af64c897820789f2c067a40d871334e
jonakls <asmot54@hotmail.com>
2022-07-17 19:28:21 -0500

    carac -> añadido el primer ejercicio del curso, impresion de textos simples

---
+++ exercise_1/main.py
@@ -0,0 +1,12 @@
+#
+# Ejercicio 1
+#
+
+# Contexto: Ejercicio simple que imprime mensajes y muestra variables definidas
+
+nombre = "Jonathan Alexander"
+apellido = "Narvaez Posada"
+
+print("Hola mundo")
+print("Mi nombre es: " + nombre + " " + apellido)
+
